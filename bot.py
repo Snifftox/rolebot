555fa4d6d042754f58fa11fafdd7d28eda31b3ee
Snifftox <91021636+Snifftox@users.noreply.github.com>
2023-08-28 10:38:16 +0200

    slash-msg

--- bot.py
+++ bot.py
@@ -9,15 +9,11 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TOKEN = TOKEN = os.getenv('DISORD_TOKEN')
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         
-    @client.event
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+    
 
         
 client = MyClient()
