a46f06f61160f4e9131a40dc7ddef5362c714d38
Christian Fröhlich <christ.froehlich@t-online.de>
2023-08-28 11:41:31 +0200

    + variable

--- User.py
+++ User.py
@@ -1,5 +1,8 @@
 import discord
 
-class user:
-    def __init__(self) -> None:
-        pass
+class user:    
+    def __init__(self, role, roleName, userName):
+        self.role = role 
+        self.roleName = roleName
+        self.userName = userName
+    
